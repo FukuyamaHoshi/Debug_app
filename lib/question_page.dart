0380bf5707efdaf8d62e0754743005a11d51f5c6
FukuyamaHoshi <star17xp@gmail.com>
2023-03-13 20:48:10 +0900

    add editor, answer sabtitle UI

--- lib/question_page.dart
+++ lib/question_page.dart
@@ -9,6 +9,8 @@ class QuestionPage extends StatelessWidget {
   String question =
       '1, 以下のJavaScriptコードを実行すると、コンソールに「false」と表示されます。バグを修正して、コンソールに「true」と表示されるようにしてください。';
 
+  // コード
+  String code = 'var x = 10;\nx = x + 10;\nconsole.log(x);';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,15 +28,18 @@ class QuestionPage extends StatelessWidget {
         elevation: 0,
         // おわるボタン設置
         actions: [
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              'おわる',
-              style: GoogleFonts.zenMaruGothic(
-                  textStyle: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: fromCssColor('#ffffff'))),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                'おわる',
+                style: GoogleFonts.zenMaruGothic(
+                    textStyle: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: fromCssColor('#ffffff'))),
+              ),
             ),
           )
         ],
@@ -42,50 +47,108 @@ class QuestionPage extends StatelessWidget {
       backgroundColor: fromCssColor('#F4F2E4'),
 
       // 出題ボックス
-      body: Container(
-        width: double.infinity,
-        height: 400,
-        color: fromCssColor('#EAE3C9'),
-        alignment: Alignment.topCenter,
-        child: Column(children: [
-          const Padding(padding: EdgeInsets.only(top: 10)),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 430,
+            color: fromCssColor('#EAE3C9'),
+            alignment: Alignment.topCenter,
+            child: Column(children: [
+              const Padding(padding: EdgeInsets.only(top: 10)),
 
-          // もんだいテキスト
-          Text(
-            'もんだい',
-            style: GoogleFonts.zenMaruGothic(
-                textStyle: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: fromCssColor('#4E3703'))),
-          ),
+              // もんだいテキスト
+              Text(
+                'もんだい',
+                style: GoogleFonts.zenMaruGothic(
+                    textStyle: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: fromCssColor('#4E3703'))),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
 
-          // もんだい分ボックス
-          Padding(
-            padding: const EdgeInsets.only(top: 10),
-            child: Container(
-              color: Colors.red,
-              child: Container(
-                width: 380,
-                height: 150,
-                color: fromCssColor('#F4F2E4'),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
+              // もんだい文ボックス
+              Container(
+                color: Colors.red,
+                child: Container(
+                  width: 380,
+                  height: 150,
+                  color: fromCssColor('#F4F2E4'),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
 
-                  // もんだい分
-                  child: Text(
-                    question,
-                    style: GoogleFonts.zenMaruGothic(
-                        textStyle: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: fromCssColor('#77694A'))),
+                    // もんだいを表示
+                    child: Text(
+                      question,
+                      style: GoogleFonts.zenMaruGothic(
+                          textStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: fromCssColor('#77694A'))),
+                    ),
                   ),
                 ),
               ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+
+              // エディターの上タブ
+              Container(
+                width: 400,
+                height: 30,
+                color: fromCssColor('#343541'),
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    'javascript',
+                    style:
+                        TextStyle(color: fromCssColor('#ffffff'), fontSize: 13),
+                  ),
+                ),
+              ),
+
+              // エディター
+              Container(
+                width: 400,
+                height: 180,
+                color: fromCssColor('#0F161F'),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10.0, left: 20, right: 20, bottom: 10.0),
+
+                  // コードを表示
+                  child: Text(
+                    code,
+                    style: GoogleFonts.robotoMono(
+                        textStyle: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: fromCssColor('#33E51C'))),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+
+          // 答えのサブタイトル
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 18.0, top: 20.0),
+              child: Text(
+                'どれが答え？',
+                style: GoogleFonts.zenMaruGothic(
+                    textStyle: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: fromCssColor('#4E3703'))),
+              ),
             ),
-          )
-        ]),
+          ),
+
+          // 答えAのボックス(ボタンにする？？)
+        ],
       ),
     );
   }
