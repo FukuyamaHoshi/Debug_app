477a37cf330cb729024fb18904ac2397d59c7316
FukuyamaHoshi <star17xp@gmail.com>
2023-03-13 14:08:19 +0900

    conmplete couse page UI

--- lib/couse_page.dart
+++ lib/couse_page.dart
@@ -8,86 +8,87 @@ class CoursePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('codegen'),
-          backgroundColor: fromCssColor('#96D6AE'),
-          elevation: 0,
+      appBar: AppBar(
+        title: const Text('codegen'),
+        backgroundColor: fromCssColor('#96D6AE'),
+        elevation: 0,
+      ),
+      backgroundColor: fromCssColor('#F4F2E4'),
+      body: Column(children: [
+        // コースのボックス
+        Container(
+          width: double.infinity,
+          height: 64,
+          color: fromCssColor('#BDE2B8'),
+          alignment: Alignment.center,
+          child: Text(
+            'コース',
+            style: GoogleFonts.zenMaruGothic(
+                textStyle: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: fromCssColor('#4E3703'))),
+          ),
         ),
-        backgroundColor: fromCssColor('#F4F2E4'),
-        body: Column(children: [
-          // コースのボックス
-          Container(
-            width: double.infinity,
-            height: 64,
-            color: fromCssColor('#BDE2B8'),
-            alignment: Alignment.center,
-            child: Text(
-              'コース',
-              style: GoogleFonts.zenMaruGothic(
-                  textStyle: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: fromCssColor('#4E3703'))),
-            ),
-          ),
 
-          // やまの名前と詳細
-          Container(
-            width: double.infinity,
-            alignment: Alignment.centerLeft,
-            child: Column(
-              children: [
-                // やまの名前
-                SizedBox(
-                  width: double.infinity,
-                  child: Row(
-                    children: [
-                      const Padding(padding: EdgeInsets.only(left: 10.0)),
-                      Text(
-                        'Javascriptやま',
-                        style: GoogleFonts.zenMaruGothic(
-                            textStyle: TextStyle(
-                                fontSize: 35,
-                                fontWeight: FontWeight.bold,
-                                color: fromCssColor('#536C67'))),
-                      ),
-                    ],
-                  ),
+        // やまの名前と詳細
+        Container(
+          width: double.infinity,
+          alignment: Alignment.centerLeft,
+          child: Column(
+            children: [
+              // やまの名前
+              SizedBox(
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    const Padding(padding: EdgeInsets.only(left: 10.0)),
+                    Text(
+                      'Javascriptやま',
+                      style: GoogleFonts.zenMaruGothic(
+                          textStyle: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              color: fromCssColor('#536C67'))),
+                    ),
+                  ],
                 ),
-                // やまの詳細
-                SizedBox(
-                  width: double.infinity,
-                  child: Row(
-                    children: [
-                      const Padding(padding: EdgeInsets.only(left: 10.0)),
-                      Text(
-                        '初級',
-                        style: GoogleFonts.zenMaruGothic(
-                            textStyle: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.normal,
-                                color: fromCssColor('#536C67'))),
-                      ),
-                      const Padding(padding: EdgeInsets.only(left: 2.0)),
-                      const Text('ー'),
-                      const Padding(padding: EdgeInsets.only(left: 2.0)),
-                      Text(
-                        '５問',
-                        style: GoogleFonts.zenMaruGothic(
-                            textStyle: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.normal,
-                                color: fromCssColor('#536C67'))),
-                      ),
-                    ],
-                  ),
+              ),
+              // やまの詳細
+              SizedBox(
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    const Padding(padding: EdgeInsets.only(left: 10.0)),
+                    Text(
+                      '初級',
+                      style: GoogleFonts.zenMaruGothic(
+                          textStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.normal,
+                              color: fromCssColor('#536C67'))),
+                    ),
+                    const Padding(padding: EdgeInsets.only(left: 2.0)),
+                    const Text('ー'),
+                    const Padding(padding: EdgeInsets.only(left: 2.0)),
+                    Text(
+                      '５問',
+                      style: GoogleFonts.zenMaruGothic(
+                          textStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.normal,
+                              color: fromCssColor('#536C67'))),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
+        ),
 
-          // 山と強調画像
-          Stack(children: [
+        // 山と強調, 地面画像
+        Expanded(
+          child: Stack(children: [
             // 強調画像
             SizedBox(
                 width: 430,
@@ -104,7 +105,70 @@ class CoursePage extends StatelessWidget {
                   'images/javascript_mountain.png',
                   fit: BoxFit.contain,
                 )),
-          ])
-        ]));
+            // 木画像
+            Align(
+              alignment: const Alignment(-0.81, 0.25),
+              child: SizedBox(
+                  width: 90,
+                  height: 90,
+                  child: Image.asset(
+                    'images/tree_first.png',
+                    fit: BoxFit.contain,
+                  )),
+            ),
+            // 木画像
+            Align(
+              alignment: const Alignment(-1, 0.25),
+              child: SizedBox(
+                  width: 90,
+                  height: 90,
+                  child: Image.asset(
+                    'images/tree_second.png',
+                    fit: BoxFit.contain,
+                  )),
+            ),
+            // 地面画像
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                  width: 430,
+                  height: 430,
+                  child: Image.asset(
+                    'images/zimen.png',
+                    fit: BoxFit.contain,
+                  )),
+            ),
+          ]),
+        ),
+      ]),
+
+      // ボタン
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 35),
+        child: SizedBox(
+          height: 73,
+          width: 329,
+          child: OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              side: const BorderSide(color: Colors.white, width: 4),
+              backgroundColor: fromCssColor('#90CCC9'),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+            child: Text(
+              'はじめる',
+              style: GoogleFonts.zenMaruGothic(
+                  textStyle: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: fromCssColor('#ffffff'))),
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
   }
 }
