3e85a09e8b267e67036170f23e94d4f200e2c971
FukuyamaHoshi <star17xp@gmail.com>
2023-03-26 11:00:47 +0900

    fix splash color and couse page image and colors

--- lib/couse_page.dart
+++ lib/couse_page.dart
@@ -18,150 +18,150 @@ class CousePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(
           'codegen',
-          style: GoogleFonts.zenMaruGothic(
+          style: GoogleFonts.lato(
               textStyle: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
                   color: fromCssColor('#ffffff'))),
         ),
-        backgroundColor: fromCssColor('#96D6AE'),
+        backgroundColor: fromCssColor('#121517'),
         elevation: 0,
         automaticallyImplyLeading: false, // もどるボタンを許可しない
       ),
-      backgroundColor: fromCssColor('#F4F2E4'),
-      body: Column(children: [
-        // コースのボックス
-        Container(
-          width: double.infinity,
-          height: 64,
-          color: fromCssColor('#BDE2B8'),
-          alignment: Alignment.center,
-          child: Text(
-            'コース',
-            style: GoogleFonts.zenMaruGothic(
-                textStyle: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: fromCssColor('#4E3703'))),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [fromCssColor('#567582'), fromCssColor('#D6D3BD')],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: const [0.2, 0.8]),
+        ),
+        child: Column(children: [
+          // コースのボックス
+          Container(
+            width: double.infinity,
+            height: 64,
+            color: fromCssColor('#567582'),
+            alignment: Alignment.center,
+            child: Text(
+              'コース',
+              style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.normal,
+                      color: fromCssColor('#ffffff'))),
+            ),
           ),
-        ),
 
-        // やまの名前と詳細
-        Container(
-          width: double.infinity,
-          alignment: Alignment.centerLeft,
-          child: Column(
-            children: [
-              // やまの名前
-              SizedBox(
-                width: double.infinity,
-                child: Row(
-                  children: [
-                    const Padding(padding: EdgeInsets.only(left: 10.0)),
-                    Text(
-                      'Javascriptやま',
-                      style: GoogleFonts.zenMaruGothic(
-                          textStyle: TextStyle(
-                              fontSize: 35,
-                              fontWeight: FontWeight.bold,
-                              color: fromCssColor('#536C67'))),
-                    ),
-                  ],
+          // やまの名前と詳細
+          Container(
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            child: Column(
+              children: [
+                // やまの名前
+                SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      const Padding(padding: EdgeInsets.only(left: 10.0)),
+                      Text(
+                        'Javascriptやま',
+                        style: GoogleFonts.lato(
+                            textStyle: TextStyle(
+                                fontSize: 35,
+                                fontWeight: FontWeight.w500,
+                                color: fromCssColor('#ffffff'))),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              // やまの詳細
-              SizedBox(
-                width: double.infinity,
-                child: Row(
-                  children: [
-                    const Padding(padding: EdgeInsets.only(left: 10.0)),
-                    Text(
-                      '初級',
-                      style: GoogleFonts.zenMaruGothic(
-                          textStyle: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.normal,
-                              color: fromCssColor('#536C67'))),
-                    ),
-                    const Padding(padding: EdgeInsets.only(left: 2.0)),
-                    const Text('ー'),
-                    const Padding(padding: EdgeInsets.only(left: 2.0)),
-                    Text(
-                      '５問',
-                      style: GoogleFonts.zenMaruGothic(
-                          textStyle: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.normal,
-                              color: fromCssColor('#536C67'))),
-                    ),
-                  ],
+                // やまの詳細
+                SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      const Padding(padding: EdgeInsets.only(left: 10.0)),
+                      Text(
+                        '初級',
+                        style: GoogleFonts.lato(
+                            textStyle: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                                color: fromCssColor('#ffffff'))),
+                      ),
+                      const Padding(padding: EdgeInsets.only(left: 2.0)),
+                      Text(
+                        'ー',
+                        style: TextStyle(color: fromCssColor('#ffffff')),
+                      ),
+                      const Padding(padding: EdgeInsets.only(left: 2.0)),
+                      Text(
+                        '５問',
+                        style: GoogleFonts.lato(
+                            textStyle: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                                color: fromCssColor('#ffffff'))),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
 
-        // 山と強調, 地面画像
-        Expanded(
-          child: Stack(children: [
-            // 強調画像
-            Align(
-              alignment: Alignment.topCenter,
-              child: SizedBox(
-                  width: 430,
-                  height: 430,
-                  child: Image.asset(
-                    'images/javascript_hallo.png',
-                    fit: BoxFit.contain,
-                  )),
-            ),
-            // 山画像
-            Align(
-              alignment: const Alignment(0, -0.9),
-              child: SizedBox(
-                  width: 465,
-                  height: 465,
-                  child: Image.asset(
-                    'images/javascript_mountain.png',
-                    fit: BoxFit.contain,
-                  )),
-            ),
-            // 木画像
-            Align(
-              alignment: const Alignment(-0.81, 0.25),
-              child: SizedBox(
-                  width: 90,
-                  height: 90,
-                  child: Image.asset(
-                    'images/tree_first.png',
-                    fit: BoxFit.contain,
-                  )),
-            ),
-            // 木画像
-            Align(
-              alignment: const Alignment(-1, 0.25),
-              child: SizedBox(
-                  width: 90,
-                  height: 90,
-                  child: Image.asset(
-                    'images/tree_second.png',
-                    fit: BoxFit.contain,
-                  )),
-            ),
-            // 地面画像
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                  width: 465,
-                  height: 465,
-                  child: Image.asset(
-                    'images/zimen.png',
-                    fit: BoxFit.contain,
-                  )),
-            ),
-          ]),
-        ),
-      ]),
+          // 山と強調, 地面画像
+          Expanded(
+            child: Stack(children: [
+              // 山画像
+              Align(
+                alignment: const Alignment(0, -0.9),
+                child: SizedBox(
+                    width: 465,
+                    height: 465,
+                    child: Image.asset(
+                      'images/javascript_mountain.png',
+                      fit: BoxFit.contain,
+                    )),
+              ),
+              // 木画像
+              Align(
+                alignment: const Alignment(-0.81, 0.3),
+                child: SizedBox(
+                    width: 90,
+                    height: 90,
+                    child: Image.asset(
+                      'images/tree_third.png',
+                      fit: BoxFit.contain,
+                    )),
+              ),
+              // 木画像
+              Align(
+                alignment: const Alignment(-1, 0.3),
+                child: SizedBox(
+                    width: 90,
+                    height: 90,
+                    child: Image.asset(
+                      'images/tree_third.png',
+                      fit: BoxFit.contain,
+                    )),
+              ),
+              // 地面画像
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                    width: 465,
+                    height: 465,
+                    child: Image.asset(
+                      'images/ground_second.png',
+                      fit: BoxFit.contain,
+                    )),
+              ),
+            ]),
+          ),
+        ]),
+      ),
 
       // ボタン
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -170,21 +170,20 @@ class CousePage extends StatelessWidget {
         child: SizedBox(
           height: 73,
           width: 329,
-          child: OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              side: const BorderSide(color: Colors.white, width: 4),
-              backgroundColor: fromCssColor('#90CCC9'),
+          child: TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: fromCssColor('#ffffff'),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50),
+                borderRadius: BorderRadius.circular(5),
               ),
             ),
             child: Text(
               'はじめる',
-              style: GoogleFonts.zenMaruGothic(
+              style: GoogleFonts.lato(
                   textStyle: TextStyle(
                       fontSize: 25,
                       fontWeight: FontWeight.bold,
-                      color: fromCssColor('#ffffff'))),
+                      color: fromCssColor('#373737'))),
             ),
             onPressed: () async {
               // Firebaseからデータ数を取得
